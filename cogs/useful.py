ebc5e446c100490977ca98d2a86fc4df868b2af5
Dastan <68908934+Dositan@users.noreply.github.com>
2020-12-18 23:03:35 +0600

    ✅ Added password generator command.

--- cogs/useful.py
+++ cogs/useful.py
@@ -25,6 +25,38 @@ class Useful(commands.Cog, command_attrs=dict(cooldown=commands.Cooldown(1, 10, 
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def gen_password(self, ctx, length: int, uppercase: bool = False, digit: bool = False, chars: bool = False):
+        '''
+        Basic Password Generator.
+        Makes a password with given settings using random.
+        '''
+        password = ''
+        lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+        uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        digits = '0123456789'
+        additional_chars = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+        for _ in range(length):
+            if uppercase is True and digit is False and chars is False:
+                password += random.choice((lowercase_letters + uppercase_letters))
+
+            if uppercase is True and digit is True and chars is False:
+                password += random.choice((lowercase_letters + uppercase_letters + digits))
+
+            if uppercase is False and digit is True and chars is True:
+                password += random.choice((lowercase_letters + digits + additional_chars))
+
+            if uppercase is False and digit is False and chars is True:
+                password += random.choice((lowercase_letters + additional_chars))
+
+            if uppercase is True and digit is True and chars is True:
+                password += random.choice((lowercase_letters + uppercase_letters + digits + additional_chars))
+
+            else:
+                password += random.choice(lowercase_letters)
+
+        await ctx.author.send(f'Generated password is: ```{password}```')
+
     @commands.command(aliases=['src'])
     async def source(self, ctx, *, command: str = None):
         '''
