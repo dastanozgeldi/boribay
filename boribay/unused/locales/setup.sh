7e0cf39f4e78d9e3c1cca59accfa0012af2c72fc
Dastan <68908934+Dositan@users.noreply.github.com>
2021-05-15 19:56:09 +0600

    ✅ Useless stuff delete.

--- boribay/unused/locales/setup.sh
+++
@@ -1,8 +0,0 @@
-xgettext --files-from files.in --from-code utf-8 --add-comments --output messages.pot
-
-for locale in */; do
-	file="$locale/LC_MESSAGES/boribay"
-
-	msgmerge --update --no-fuzzy-matching --backup off "$file.po" messages.pot
-
-	msgfmt "$file.po" --output-file "$file.mo"; done
