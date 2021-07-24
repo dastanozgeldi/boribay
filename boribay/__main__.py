da4a8fe571981cfdb5e50c2cd9f50209695fca08
Dastan <68908934+Dositan@users.noreply.github.com>
2021-07-24 15:49:43 +0600

    🎀 Tiny changes.

--- boribay/__main__.py
+++ boribay/__main__.py
@@ -14,31 +14,20 @@ def main() -> None:
     """The main function of the bot that exactly manages the Boribay app."""
     init_logging(level=logging.INFO)
 
-    bot = None
     args = parse_flags(sys.argv[1:])
+    bot = Boribay(cli_flags=args)
     parse_single_flags(args)
 
     try:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        # Initializing the bot class, starting.
-        bot = Boribay(cli_flags=args)
         loop.create_task(bot.start())
         loop.run_forever()
-
-    except KeyboardInterrupt:  # CTRL-C used.
-        log.warning('Please do not use Ctrl+C to Shutdown the bot! (attempting to die gracefully...)')
-        log.error('Received KeyboardInterrupt, treating as interrupt')
-
-    except SystemExit as exc:  # Exit by Python.
-        log.info(f'Shutting down with exit code: {exc.code}')
-
     except Exception as exc:  # Unhandled exception.
-        log.exception(f'Unexpected exception ({type(exc)}): ', exc_info=exc)
-
+        log.exception(f'Unhandled exception ({type(exc)}): ', exc_info=exc)
     finally:
         loop.run_until_complete(loop.shutdown_asyncgens())
-        log.info('Please wait, cleaning up a bit more')
+        log.info('Cleaning up...')
         loop.run_until_complete(asyncio.sleep(2))
         asyncio.set_event_loop(None)
         loop.stop()
